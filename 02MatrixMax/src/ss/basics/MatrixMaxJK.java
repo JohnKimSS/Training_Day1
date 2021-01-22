07d2340778318739c58fd9ef8972bda39afa0aad
johnk9000 <33733453+johnk9000@users.noreply.github.com>
2021-01-21 21:14:22 -0600

    Boiler plate for 2D array problem

---
+++ 02MatrixMax/src/ss/basics/MatrixMaxJK.java
@@ -0,0 +1,12 @@
+/* @Author John Kim
+Find the maximum value of an n x m array and return it's indicies
+*/
+package com.ss.basics.matrixmax;
+
+public class MatrixMax {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
