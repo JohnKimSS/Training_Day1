d11a19c6bd3a539a711d2e195adf28166fb62d8f
johnk9000 <33733453+johnk9000@users.noreply.github.com>
2021-01-21 21:42:58 -0600

    Attempted to create array with random numbers

--- 02MatrixMax/src/ss/basics/MatrixMaxJK.java
+++ 02MatrixMax/src/ss/basics/MatrixMaxJK.java
@@ -6,7 +6,17 @@ package com.ss.basics.matrixmax;
 public class MatrixMax {
 
     public static void main(String[] args) {
-        
+
+        int n = 5;
+        int m = 5;
+        int [][] matrix = new int[n][m];
+        // Build array of n x m with random integers
+        for(int i=0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                matrix[i][j] = (int)(Math.random() * 100);   
+            }
+        }
+        System.out.println(matrix);
     }
 
 }
