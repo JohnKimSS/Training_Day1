798c1c8cf64fe582b71beb85cf0f177b5f18b7d5
johnk9000 <33733453+johnk9000@users.noreply.github.com>
2021-01-21 21:51:16 -0600

    Code is acting out of wack

--- 02MatrixMax/src/ss/basics/MatrixMaxJK.java
+++ 02MatrixMax/src/ss/basics/MatrixMaxJK.java
@@ -1,18 +1,13 @@
-/* @Author John Kim
-Find the maximum value of an n x m array and return it's indicies
-*/
-package com.ss.basics.matrixmax;
-
 public class MatrixMax {
 
     public static void main(String[] args) {
-
+        
         int n = 5;
         int m = 5;
-        int [][] matrix = new int[n][m];
-        // Build array of n x m with random integers
+        int [][] matrix = new int[5][5];
+        //Build array of n x m with random integers
         for(int i=0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
+            for(int j=0; j < m; j++) {
                 matrix[i][j] = (int)(Math.random() * 100);   
             }
         }
