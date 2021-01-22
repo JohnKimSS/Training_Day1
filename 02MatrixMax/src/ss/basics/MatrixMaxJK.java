a6c1aea7d672585c2b4250775ab3df6de2f316bb
johnk9000 <33733453+johnk9000@users.noreply.github.com>
2021-01-21 22:25:02 -0600

    Was having issue with IDE, wrote in other repo and copy pasta working code into here

--- 02MatrixMax/src/ss/basics/MatrixMaxJK.java
+++ 02MatrixMax/src/ss/basics/MatrixMaxJK.java
@@ -1,17 +1,28 @@
-public class MatrixMax {
-
+public class MatrixMaxJK {
     public static void main(String[] args) {
-        
         int n = 5;
         int m = 5;
-        int [][] matrix = new int[5][5];
+        int [][] matrix = new int[n][m];
+        int maxNum = 0;
+        int [] indices = {0, 0};
         //Build array of n x m with random integers
         for(int i=0; i < n; i++) {
             for(int j=0; j < m; j++) {
                 matrix[i][j] = (int)(Math.random() * 100);   
             }
         }
-        System.out.println(matrix);
+        // Find the max value of the array and save the indices
+        for(int i=0; i < n; i++) {
+            for(int j=0; j < m; j++) {
+                if(matrix[i][j] > maxNum) {
+                    maxNum = matrix[i][j];
+                    indices[0] = i;
+                    indices[1] = j;
+                } 
+            }
+        }
+    System.out.println("Max val = " + maxNum + ", (n,m) = " + indices[0] + ", " + indices[1]);
     }
+
 
 }
