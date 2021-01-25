4127bf1cca34b68780251e8eaec3b45273f21071
John Kim <johnk9000@gmail.com>
2021-01-25 10:23:40 -0600

    Starting letter count assignment

---
+++ 03FileLetterCount/src/ss/basic/LetterCountJK.java
@@ -0,0 +1,17 @@
+public class LetterCount {
+
+    public static void main(String[] args) {
+
+        string letter = "e";
+        LetterCount lc = new LetterCount("C:\\Users\\johnk\\OneDrive\\Documents\\GitHub\\Practice\\React\\Training_Day1\\03TextAppend\\src\\ss\\basics\\b-script.txt", letter);
+        System.out.println("count")
+    }
+
+    LetterCount(Path file, char lookFor) {
+        this.file = file;
+        this.lookFor = lookFor;
+    }
+
+
+    
+}
