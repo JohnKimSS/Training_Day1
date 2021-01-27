3210d077ac125a924b0728dbda671d2c78d5635d
johnk9000 <33733453+johnk9000@users.noreply.github.com>
2021-01-26 20:55:16 -0600

    Fleshed out excecutable of the code

--- 04ProducerConsumer/ProdConJK.java
+++ 04ProducerConsumer/ProdConJK.java
@@ -2,7 +2,34 @@ import java.util.LinkedList;
 
 public class ProdCom {
     public static void main(String[] args) throws InterruptedException {
-
+        final ProdCom pc = new ProdCom();
+        // Consumer Thread
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    pc.consume();
+                } catch (InterruptedException err) {
+                    err.printStackTrace();
+                }
+            }
+        });
+        // Producer Thread
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    pc.produce();
+                } catch (InterruptedException err) {
+                    err.printStackTrace();
+                }
+        });
+        // Starting the threads
+        t1.start();
+        t2.start();
+        // using 'join' so that current thread invokes method other thread
+        t1.join();
+        t2.join();
     }
 }
 
